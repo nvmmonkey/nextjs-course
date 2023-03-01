e72b2701349305c59ac0471cf13ccdc6c9dd749f
Kit <nvmmonkey@github.com>
2023-03-01 16:47:15 +0800

    ⚛️Event Page: route/push/query/filter/button/handler

---
+++ pages/events/index.js
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { getAllEvents } from "../../dummy-data";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
+  
+
+  return (
+    <div>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </div>
+  );
+}
+
+export default AllEventsPage;
