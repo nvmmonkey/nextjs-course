fd20d111a9bd8306081480f75b1ed53faa432614
Kit <nvmmonkey@github.com>
2023-03-04 02:49:18 +0800

    Client side data fetching with useSWR & w/o useEffect

---
+++ pages/last-sales.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function LastSalePage() {
+  const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const dataURL =
+    "https://react-dummydb-default-rtdb.firebaseio.com/sales.json";
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(dataURL, fetcher);
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://react-dummydb-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Fail to load!</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => {
+        return (
+          <li key={sale.id}>
+            {sale.username} - ${sale.volume}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+export default LastSalePage;
