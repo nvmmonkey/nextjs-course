f3da7cc9fb7ea9b4ab826bc91043caef84542c02
Kit <nvmmonkey@github.com>
2023-03-08 02:47:44 +0800

    Add form submit handler for input data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackIputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackIputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address: </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="your@email.com"
+            ref={emailInputRef}
+          />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback: </label>
+          <textarea
+            rows="5"
+            id="feedback"
+            placeholder="..."
+            ref={feedbackIputRef}
+          />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
