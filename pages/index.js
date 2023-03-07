cf3f3d2511e3070c11d6eaf875598e52179a5390
Kit <nvmmonkey@github.com>
2023-03-08 02:28:41 +0800

    Initialized

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
-import Head from "next/head";
-import Link from "next/link";
-import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../components/helper/api-utils";
-
-function HomePage(props) {
+function HomePage() {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta
-          name="description"
-          content="Find a lot of great events that allow you to evolve!"
-        />
-      </Head>
-      <EventList items={props.featuredEvents} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      featuredEvents: featuredEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
