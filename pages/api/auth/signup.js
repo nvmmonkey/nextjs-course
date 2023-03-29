a01d28d9af5ca9d05ed43e5c6d8183dfdfb5a426
Kit <nvmmonkey@github.com>
2023-03-30 01:24:31 +0800

    Add form submit & POST method for the API

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,17 +2,26 @@ import { hashPassword } from "../../../helper/auth-utils";
 import { ConnectMongoDB, insertUser } from "../../../helper/db-utils";
 
 async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
   const data = req.body;
   const { email, password } = data;
 
-  if (!email || !email.includes("@") || !password || !email.trim().length < 7) {
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
     res.status(422).json({
       message: "Invalid input -- password should be at least 7 characters!",
     });
     return;
   }
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   //create an storing object
   const newUser = {
