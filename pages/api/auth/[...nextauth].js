294a385e2a93118ad672ea99d0140a2f3d66d2bf
Kit <nvmmonkey@github.com>
2023-03-30 02:14:53 +0800

    Add next-auth V3 + [...nexauth].js for JWT on the backend

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import NextAuth from "next-auth/next";
+import Providers from "next-auth/providers";
+import { verifyPassword } from "../../../helper/auth-utils";
+import { ConnectMongoDB } from "../../../helper/db-utils";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        try {
+          await ConnectMongoDB;
+        } catch (error) {
+          throw new Error("Failing connecting to user DB!");
+        }
+
+        const exitingUser = await findUser(credentials.email);
+        if (!exitingUser) {
+          mongoose.connection.close();
+          throw new Error("No user founded!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          exitingUser.password
+        );
+
+        if (!isValid) {
+          mongoose.connection.close();
+          throw new Error("Password is invalid, authentication fail!");
+        }
+
+        mongoose.connection.close();
+        return { email: exitingUser.email };
+      },
+    }),
+  ],
+});
